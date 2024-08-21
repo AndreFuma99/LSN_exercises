163fabd079831cae91644951f6ef8cfc7cee4941
AndreaFuma99 <afuma99@gmail.com>
2024-08-21 16:39:04 +0200

    Aggiungo una grande quantità di file

---
+++ Esercitazione_4/Esercizio_04_2/Solid_Phase/nsl_simulator/NSL_SIMULATOR/SOURCE/NSL_SIMULATOR.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "system.h"
+
+using namespace std;
+
+int main (int argc, char *argv[]){
+
+  int nconf = 1;
+  System SYS;
+  SYS.initialize();
+  SYS.initialize_properties();
+  SYS.block_reset(0);
+  
+  // Per EQUILIBRAZIONE: faccio andare la simulazione con 10000 blocchi con 1 step; stampo T istantanea e guardo dopo quanti blocchi converge: questo è il "tempo di equilibrazione". Cambio e riavvio
+  
+  for(int i=0; i < 20000; i++){   // 2000 è il "tempo di equilibrazione"
+     SYS.step();
+     SYS.measure();
+  } 
+  
+  // Ora inizia la simulazione
+  for(int i = 0; i < SYS.get_nbl(); i++){ //loop over blocks
+    SYS.block_reset(i+1);
+    for(int j = 0; j < SYS.get_nsteps(); j++){ //loop over steps in a block
+      SYS.step();
+      SYS.measure();
+        if(j % 10 == 0){
+       // SYS.write_XYZ(nconf); //Write actual configuration in XYZ format //Commented to avoid "filesystem full"!
+          nconf++;
+        }
+    }
+    SYS.averages(i+1);
+    //SYS.block_reset(i+1);
+  }
+  SYS.finalize();
+
+  return 0;
+}
+
+
